977d30876fc205e95420e8ef0fd3e9412857f29d
Kelly Ryan <44586184+Kelly-Ryan@users.noreply.github.com>
2019-01-17 17:35:29 +0000

    Add files via upload

---
+++ JFT3Ex1.java
@@ -0,0 +1,40 @@
+public class JFT3Ex1 {
+
+	public static void main (String[] args) {
+		
+		String firstName = "John";
+		char middleInitial = 'B';
+		String lastName = "Smith";
+		byte age = 33;
+		String occupation = "Java Programmer";
+		float height = 6.2f;
+		float weight = 10;
+		boolean isSmoker = true;
+		
+		System.out.println(firstName);
+		System.out.println(middleInitial);
+		System.out.println(lastName);
+		System.out.println(age);
+		System.out.println(occupation);
+		System.out.println(height);
+		System.out.println(weight);
+		System.out.println(isSmoker);
+
+	}
+
+/*
+Write a Java program to store details about your profile.
+In the main method, create the following variables.
+ firstName String
+ middleInitial char
+ lastName String
+ age byte
+ occupation String
+ height float
+ weight float
+ isSmoker boolean
+In the main method, display the values stored in the variables.
+Save the program as JFT3Ex1.java
+*/
+	
+}
---
+++ JFT3Ex2.java
@@ -0,0 +1,37 @@
+public class JFT3Ex2 {
+
+	public static void main (String[] args) {
+
+	String firstName = "John";
+	char middleInitial = 'B';
+	String lastName = "Hayes";
+	final String ppsNo = "5647314S";
+	int age = 34;
+	String department = "IT";
+	byte performanceRating = 3;
+	float basicSalary = 45909.56F;
+	boolean bonusAwarded = true;
+	double bonus = 10000.04;
+	double totalSalary = (basicSalary+bonus);
+	
+	System.out.println("First Name: "+firstName);
+	System.out.println("Middle Initial: "+middleInitial);
+	System.out.println("Last Name: "+lastName);
+	System.out.println("PPS No. "+ppsNo);
+	System.out.println("Age: "+age);
+	System.out.println("Department: "+department);
+	System.out.println("Performance Rating: "+performanceRating);
+	System.out.println("Basic Salary: "+basicSalary);
+	System.out.println("Bonus Awarded: "+bonusAwarded);
+	System.out.println("Bonus: "+bonus);
+	System.out.println("Total Salary: "+totalSalary);
+	
+	}
+
+/*
+Write a Java program using variables to store information about an employee.
+In the main method, display the values stored in the variables.
+Save the program as JFT3Ex2.java
+*/
+	
+}
